c445e801d309aa8ab4b059ac9304426a00081f5f
Ana Flavia <afrodrigues38@gmail.com>
2020-08-05 19:11:22 -0300

    Exercício completo

--- semana4/objetos/blog/index.js
+++ semana4/objetos/blog/index.js
@@ -1,38 +1,33 @@
 
-
+//criando a array com as informações digitadas pelo usuário
 function criaArray(){
-
+    //capturar os valores digitados em cada campo do formulario
     const tit = document.getElementById("titulo-post").value
     const aut = document.getElementById("autor-post").value
     const cont= document.getElementById("conteudo-post").value
-    
+    const imag = document.getElementById("imagem-post").value
+    //criando o objeto com as informações coletadas
     const blogInfo = {
         titulo:tit,
         autor:aut,
         conteudo:cont
     }
-    const container = document.getElementById("container-de-posts")
-    container.innerHTML += `<div><h2>${blogInfo},</h2><p></p></div>` 
+    
+    //imprimindo valores numa div do html
     const arrayFinal = []
     arrayFinal.push(blogInfo)
-    console.log(blogInfo)
-    //console.log(arrayFinal)
-     tit.value=""
-     aut.value=""
-     cont.value=""
-    
-}
-
-function armazenaDados(){
-    
-    
-}
-function apertouEnter(evento) {
-    if (evento.key === 'Enter'){
-        identificarImagem()
+   
+    const container = document.getElementById("container-de-posts")
+    if (imag.includes('.png') || imag.includes('.jpg')){
+        //direciona para outra página
+     // window.location.href="informacao.html"
+      container.innerHTML += `<div><h2>${tit}</h2><p>${aut}</p><p>${cont}</p><p><img src=${imag}></p></div>` 
+    } else {
+        alert('Link incorreto! Insira imagem válida')
     }
-}
-
-function apertouBotao(evento) {
-    identificarImagem()
+    //limpando os campos 
+    tit.value=""
+    aut.value=""
+    cont.value=""
+    
 }

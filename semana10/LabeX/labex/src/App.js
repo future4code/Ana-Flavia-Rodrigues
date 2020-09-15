06bebe75f631f6fd59fbe643559ca4e4a735e0dd
Flavia <afrodrigues38@gmail.com>
2020-09-15 16:39:55 -0300

    inclusão das rotas com o componente Route

--- semana10/LabeX/labex/src/App.js
+++ semana10/LabeX/labex/src/App.js
@@ -1,25 +1,12 @@
 import React from 'react';
 import styled from 'styled-components'
-import HomePage from './components/HomePage/HomePage';
-import LoginPage from './components/HomePage/LoginPage/LoginPage';
-import AplicationFormPage from './components/AplicationFormPage/AplicationFormPage';
-import CreateTripePage from './components/CreateTripePage/CreateTripePage';
-import ListTripsPage from './components/ListTripsPage/ListTripsPage';
-import RouterPage from './components/RouterPage/RouterPage'; 
-import TripDetailsPage from './components/TripDetailsPage/TripDetailsPage';
+import Router from './router/Router'
 
 
 function App() {
   return (
     <AppContainer >
-      <HomePage>
-        <LoginPage/>
-      </HomePage>
-      <AplicationFormPage/>
-      <CreateTripePage/>
-      <ListTripsPage/>
-      <RouterPage/>
-      <TripDetailsPage/>
+     <Router/>
     </AppContainer>
   );
 }
---
+++ semana10/LabeX/labex/src/router/Router.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import HomePage from '../screens/HomePage/HomePage'
+import AplicationFormPage from '../screens/AplicationFormPage/AplicationFormPage';
+import CreateTripePage from '../screens/CreateTripePage/CreateTripePage';
+import ListTripsPage from '../screens/ListTripsPage/ListTripsPage';
+import LoginPage from '../screens/LoginPage/LoginPage';
+import RouterPage from '../screens/RouterPage/RouterPage';
+import TripDetailsPage from '../screens/TripDetailsPage/TripDetailsPage';
+
+const Router = () => {
+  return(
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/">
+                <HomePage/>
+            </Route>
+            <Route exact path="/formulario">
+                <AplicationFormPage/>
+            </Route>
+            <Route exact path="/viagens">
+                <CreateTripePage/>
+            </Route>
+            <Route exact path="/lista_viagens">
+                <ListTripsPage/>
+            </Route> 
+            <Route exact path="/login">
+                <LoginPage/>
+            </Route>
+            <Route exact path="/roteiro">
+                <RouterPage/>
+            </Route>
+            <Route exact path="/detalhes">
+                <TripDetailsPage/>
+            </Route>
+            <Route>
+                <div>Página não encontrada (404) :(</div>
+            </Route>
+        </Switch>
+    </BrowserRouter>
+  )}
+export default Router;

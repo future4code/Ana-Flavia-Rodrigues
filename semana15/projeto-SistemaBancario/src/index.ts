5f6d875b0e413ba04bd87f8315b7bcdc0ba9461a
Flavia <afrodrigues38@gmail.com>
2020-11-26 10:26:38 -0300

    projeto da semana15

--- semana15/projeto-SistemaBancario/src/index.ts
+++ semana15/projeto-SistemaBancario/src/index.ts
@@ -7,6 +7,20 @@ const app: Express = express()//chamar a função express
 app.use(express.json())
 app.use(cors());
 
+app.get("/users/:123123",(req: Request, res: Response)=>{
+
+    try {
+        
+        const balanceByCpf = accounts.filter((account)=>account.CPF===req.params.CPF)
+        console.log(balanceByCpf)
+        res.status(200).send(balanceByCpf);
+    }catch(error){
+        res.status(400).send({
+            message: "Error searching for CPF"
+        });
+    }
+})
+
 app.post("/users/create",(req: Request, res: Response)=>{
     try{
         const {name, CPF, dateOfBirthAsString}=req.body//tudo isso vem do body. Aqui esta uma desestruturação
@@ -14,7 +28,7 @@ app.post("/users/create",(req: Request, res: Response)=>{
         const [day, month, year]= dateOfBirthAsString.split("/")//quebrando a data nas barras para que fique um array [01,01,2020]
 
         const dateOfBirth: Date = new Date(`${year}-${month}-${day}`)
-        //validar as emntradas da requisição
+        // validar as emntradas da requisição
         const ageInMilisseconds:number = new Date().getTime() - dateOfBirth.getTime()//ou Date.now() forma reduzida
 
         const ageInYears:number = ageInMilisseconds / 1000 / 60 / 60 / 24 / 365 //transforma milisegundos em segundos/minutos/hora/dias/anos
@@ -22,10 +36,9 @@ app.post("/users/create",(req: Request, res: Response)=>{
         if (ageInYears < 18){
             res.statusCode = 406
             throw new Error("Idade deve ser maior que 18 anos")
-        }
+            }
 
-        //consultar ou alterar a base de dados
-
+        // consultar ou alterar a base de dados
         accounts.push({ 
             name, //nome valor são iguais, então pode escrever uma vez só
             CPF, 
@@ -34,7 +47,7 @@ app.post("/users/create",(req: Request, res: Response)=>{
             statement: []
         })
 
-        //validar os resulçtados da consulta
+        //validar os resultados da consulta
         //enviar respostas
         res.status(201).send("Conta criada com sucesso!")//
     }catch(error){
